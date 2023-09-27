bd2b59e802e156eddc6b3d9659796dc6136f7480
samgreen <2300389@abertay.ac.uk>
2023-09-27 12:48:37 +0100

    Inherits from cell

--- Assets/Scripts/Cells/RunAway.cs
+++ Assets/Scripts/Cells/RunAway.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RunAway : MonoBehaviour
+public class RunAway : Cell
 {
     // Normal Movements Variables
 
