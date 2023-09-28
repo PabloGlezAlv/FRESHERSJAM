e1c7731c31acae66ade15f2e12ba83f0d8f4107d
remy5623 <guitarguru767@gmail.com>
2023-09-28 21:06:30 +0100

    Added the new Input System
    Everyone - add the Input System in Package Manager.

--- Assets/Scripts/Menus/StartMenu.cs
+++ Assets/Scripts/Menus/StartMenu.cs
@@ -7,7 +7,8 @@ using UnityEngine.SceneManagement;
 public class StartMenu : MonoBehaviour
 {
 
-    public string firstScene = "";
+    public string firstScene;
+    public string settingsScene;
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +26,11 @@ public class StartMenu : MonoBehaviour
     public void OnGameStart()
     {
         SceneManager.LoadScene(firstScene);
+    }
+
+    public void GoToSettings()
+    {
+        SceneManager.LoadScene(settingsScene);
     }
 
     // Quits to desktop. If testing in the editor, ends play mode.
