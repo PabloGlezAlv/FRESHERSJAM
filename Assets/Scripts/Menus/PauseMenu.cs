b5cb402480fd1bafa94bbd2b64b9532cf1fb7128
remy5623 <guitarguru767@gmail.com>
2023-09-28 22:13:24 +0100

    Added the ability to pause, and a Pause Menu.
    This required adding a script to the Player prefab.

---
+++ Assets/Scripts/Menus/PauseMenu.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    // Designer set
+    public string startMenuScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Unpause the game
+    public void OnResume()
+    {
+        Time.timeScale = 1; // Resume the game
+        Destroy(this.gameObject);   // Take the menu off screen
+    }
+
+    // Return to the Start Menu
+    public void OnQuitToMenu()
+    {
+        Time.timeScale = 1; // Resume game time before quitting to the menu.
+        SceneManager.LoadScene(startMenuScene);
+    }
+
+    // Close the game. If playing in the editor, end play mode.
+    public void OnQuitToDesktop()
+    {
+        if (Application.isEditor)
+        {
+            EditorApplication.ExitPlaymode();
+        }
+        else
+            Application.Quit();
+    }
+}
