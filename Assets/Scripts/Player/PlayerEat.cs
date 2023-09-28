2534f6b2e339ab5976670f677ccdafb56931e9c7
samgreen <2300389@abertay.ac.uk>
2023-09-28 18:34:11 +0100

    Basic room

--- Assets/Scripts/Player/PlayerEat.cs
+++ Assets/Scripts/Player/PlayerEat.cs
@@ -6,7 +6,7 @@ public class PlayerEat : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log(collision.name);
+        // Debug.Log(collision.name);
 
         if(collision.gameObject.GetComponent<Cell>())
         {
--- Assets/Scripts/Player/PlayerMov.cs
+++ Assets/Scripts/Player/PlayerMov.cs
@@ -25,9 +25,9 @@ public class PlayerMov : MonoBehaviour
         // Body rotation
         Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        //transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
-        Debug.Log(Input.mousePosition);
+        //Debug.Log(Input.mousePosition);
     }
 
     private void FixedUpdate()
