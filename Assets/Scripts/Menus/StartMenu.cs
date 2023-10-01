6abaa34f5db5edaa8a3fa19841e008427e4949ea
PabloGlezAlv <62878159+PabloGlezAlv@users.noreply.github.com>
2023-10-01 16:46:54 +0100

    first sound

--- Assets/Scripts/Menus/StartMenu.cs
+++ Assets/Scripts/Menus/StartMenu.cs
@@ -10,8 +10,7 @@ using UnityEngine.SceneManagement;
 public class StartMenu : MonoBehaviour
 {
 
-    public string firstScene;
-    public string settingsScene;
+    [SerializeField] AudioSource audio;
 
     // Start is called before the first frame update
     void Start()
@@ -29,16 +28,15 @@ public class StartMenu : MonoBehaviour
     public void OnGameStart()
     {
         SceneManager.LoadScene("SamDevScene");
+
+        audio.Play();
     }
 
-    public void GoToSettings()
-    {
-        SceneManager.LoadScene(settingsScene);
-    }
 
     // Quits to desktop. If testing in the editor, ends play mode.
     public void OnGameQuit()
     {
+        audio.Play();
         #if UNITY_EDITOR
         EditorApplication.ExitPlaymode();
         #else
